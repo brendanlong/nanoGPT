7a1a063e715d2c7ba4d1afa8a72408a84cbade35
Brendan Long <self@brendanlong.com>
2025-08-20 10:02:36 -0700

    Add gpt-2 with custom tokenization and first-letter tokens

---
+++ data/openwebtext_word_tokens_letter/prepare.py
@@ -0,0 +1,138 @@
+# saves the openwebtext dataset to a binary file for training. following was helpful:
+# https://github.com/HazyResearch/flash-attention/blob/main/training/src/datamodules/language_modeling_hf.py
+
+import os
+from tqdm import tqdm
+import numpy as np
+from datasets import load_dataset  # huggingface datasets
+from tokenizers import (
+    models,
+    normalizers,
+    pre_tokenizers,
+    trainers,
+    Tokenizer,
+    tokenizers,
+    decoders,
+)
+from transformers import PreTrainedTokenizerFast
+import pickle
+import sys
+
+# number of workers in .map() call
+# good number to use is ~order number of cpu cores // 2
+num_proc = os.cpu_count() // 2
+
+# number of workers in load_dataset() call
+# best number might be different from num_proc above as it also depends on NW speed.
+# it is better than 1 usually though
+num_proc_load_dataset = num_proc
+
+meta_file = os.path.join(os.path.dirname(__file__), "meta.pkl")
+
+if __name__ == "__main__":
+    chosen_char = sys.argv[1]
+    print("Forcing char prefix in tokenizer:", chosen_char)
+
+    # takes 54GB in huggingface .cache dir, about 8M documents (8,013,769)
+    dataset = load_dataset("openwebtext", num_proc=num_proc_load_dataset)
+
+    # owt by default only contains the 'train' split, so create a test split
+    split_dataset = dataset["train"].train_test_split(
+        test_size=0.0005, seed=2357, shuffle=True
+    )
+    split_dataset["val"] = split_dataset.pop("test")  # rename the test split to val
+
+    # this results in:
+    # >>> split_dataset
+    # DatasetDict({
+    #     train: Dataset({
+    #         features: ['text'],
+    #         num_rows: 8009762
+    #     })
+    #     val: Dataset({
+    #         features: ['text'],
+    #         num_rows: 4007
+    #     })
+    # })
+
+    if os.path.exists(meta_file):
+        with open(meta_file, "rb") as f:
+            meta = pickle.load(f)
+            enc = meta["enc"]
+
+    else:
+        # Tokenize by normalizing, lowercasing, and using individual words as tokens
+        tokenizer = Tokenizer(models.BPE())
+        tokenizer.normalizer = normalizers.Sequence(
+            [normalizers.NFD(), normalizers.Lowercase(), normalizers.StripAccents()]
+        )
+        tokenizer.pre_tokenizer = pre_tokenizers.Split(
+            # Split on digits, words and then anything else
+            pattern=tokenizers.Regex(chosen_char + r"|[0-9]|\w+|."),
+            behavior="isolated",
+        )
+        tokenizer.decoder = decoders.BPEDecoder()
+        special_tokens = ["[EOS]"]
+        trainer = trainers.BpeTrainer(special_tokens=special_tokens, vocab_size=50257)
+
+        def batch_iterator(dataset, batch_size: int = 1000):
+            for i in range(0, len(dataset), batch_size):
+                yield dataset[i : i + batch_size]["text"]
+
+        tokenizer.train_from_iterator(
+            batch_iterator(dataset["train"]), trainer, len(dataset["train"])
+        )
+        enc = PreTrainedTokenizerFast(
+            tokenizer_object=tokenizer, bos_token="[EOS]", eos_token="[EOS]"
+        )
+        print("vocab size:", enc.vocab_size)
+
+        # save the meta information as well, to help us encode/decode later
+        meta = {
+            "vocab_size": tokenizer.get_vocab_size(),
+            "enc": enc,
+        }
+        with open(os.path.join(os.path.dirname(__file__), "meta.pkl"), "wb") as f:
+            pickle.dump(meta, f)
+
+    # we now want to tokenize the dataset. first define the encoding function (gpt2 bpe)
+    def process(example):
+        ids = enc.encode(example["text"])
+        ids.extend(enc.encode("[EOS]"))  # add the end of text token
+        out = {"ids": ids, "len": len(ids)}
+        return out
+
+    # tokenize the dataset
+    tokenized = split_dataset.map(
+        process,
+        remove_columns=["text"],
+        desc="tokenizing the splits",
+        num_proc=num_proc,
+    )
+
+    # concatenate all the ids in each dataset into one large file we can use for training
+    for split, dset in tokenized.items():
+        arr_len = np.sum(dset["len"], dtype=np.uint64)
+        filename = os.path.join(os.path.dirname(__file__), f"{split}.bin")
+        dtype = np.uint16  # (can do since enc.max_token_value == 50256 is < 2**16)
+        arr = np.memmap(filename, dtype=dtype, mode="w+", shape=(arr_len,))
+        total_batches = 1024
+
+        idx = 0
+        for batch_idx in tqdm(range(total_batches), desc=f"writing {filename}"):
+            # Batch together samples for faster write
+            batch = dset.shard(
+                num_shards=total_batches, index=batch_idx, contiguous=True
+            ).with_format("numpy")
+            arr_batch = np.concatenate(batch["ids"])
+            # Write into mmap
+            arr[idx : idx + len(arr_batch)] = arr_batch
+            idx += len(arr_batch)
+        arr.flush()
+
+    # train.bin is ~17GB, val.bin ~8.5MB
+    # train has ~9B tokens (9,035,582,198)
+    # val has ~4M tokens (4,434,897)
+
+    # to read the bin files later, e.g. with numpy:
+    # m = np.memmap('train.bin', dtype=np.uint16, mode='r')
